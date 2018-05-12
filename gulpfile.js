75644669db6ecffff8f94fef2f678fdc43794be4
Filip <filip.ziljak@outlook.com>
2018-05-12 18:45:20 +0200

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+//const browserSync = require("browser-sync").create();
+const evilIcons = require("gulp-evil-icons");
+const gcmq = require("gulp-group-css-media-queries");
+
+gulp.task("readyHTML", function(){
+    gulp.src("./src/*.html")
+        .pipe(evilIcons())
+        .pipe(gulp.dest("./dist"))
+        //.pipe(browserSync.stream());
+});
+
+gulp.task("cssReady", function(){
+    gulp.src("./src/sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gcmq())
+        .pipe(cssnano())
+        .pipe(gulp.dest("./dist/css"))
+        //.pipe(browserSync.stream());
+});
+
+gulp.task("javascriptReady", function(){
+    gulp.src("./src/js/**/*.js")
+        //.pipe(uglify())
+        .pipe(gulp.dest("./dist/js"));
+});
+
+/*
+// nes ne valja
+gulp.task("browser-sync", function(){
+    browserSync.init({
+        files: ["./src/*.html"],
+        server: {
+            baseDir: "./dist",
+            directory: true
+        }
+    });
+
+    
+
+    gulp.watch("./src/sass/**/ /* *.scss", ["cssReady"]);
+    gulp.watch("./src/*.html", ["readyHTML"]); 
+    gulp.watch("./src/*.html").on("change", browserSync.reload());
+    gulp.watch("./src/js/**/ // *.js", ["javascriptReady"]);
+    /* gulp.watch("./src/img/*", ["imageMin"]);
+     /*
+    
+    
+});
+
+*/
+
+gulp.task("imageMin", function(){
+    gulp.src("./src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img"))
+        //.pipe(browserSync.stream());
+});
+
+gulp.task("watch", function(){
+    gulp.watch("./src/sass/**/*.scss", ["cssReady"]);
+    gulp.watch("./src/*.html", ["readyHTML"]); 
+    gulp.watch("./src/js/**/*.js", ["javascriptReady"]);
+    gulp.watch("./src/img/*", ["imageMin"]);
+});
+
+gulp.task("default", ["readyHTML", "cssReady", "javascriptReady", "imageMin", "watch"]);
+
+//gulp.task("default", ["readyHTML", "cssReady", "javascriptReady", "imageMin", "browser-sync"]);
+
